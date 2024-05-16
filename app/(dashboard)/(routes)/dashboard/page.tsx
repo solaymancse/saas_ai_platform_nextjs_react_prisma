20f1139a45bdac005d75525c02b730be72b5b571
solaymancse <mdsolaymancse@gmail.com>
2024-05-16 21:15:12 +0600

    commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        Landing page (Unprotected)
+      
+    </div>
+  )
+}
+
+export default LandingPage
